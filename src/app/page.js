14b4d8c358a2655bac0bbca5a6da57c8ecdda6f7
aboulmagd <aboulmagd@live.com>
2024-01-17 22:40:03 +0300

    fetch data

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,26 @@
-import React from "react";
+import Results from "@/components/Results";
 
-export default function Home() {
-  return <div>Layout</div>;
+const API_KEY = process.env.API_KEY;
+
+async function fetchMoviesList(params) {
+  const genre = params.genre || "featchTrinding";
+  const rsp = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=en-US&page=1`
+  );
+  const data = await rsp.json();
+  if (!rsp.ok) {
+    throw new Error("Fail to load data");
+  }
+  return data.results;
+}
+export default async function Home({ searchParams }) {
+  const moveslist = await fetchMoviesList(searchParams);
+  console.log(moveslist);
+  return (
+    <div>
+      <Results results={moveslist} />
+    </div>
+  );
 }
