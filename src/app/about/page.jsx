60dc2b4d5de39d2eda8160074f6db47357c0fcbb
aboulmagd <aboulmagd@live.com>
2024-01-17 11:51:22 +0300

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 gap-4 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+        ipsa laborum asperiores assumenda, neque esse, voluptate, minus incidunt
+        culpa quidem necessitatibus cumque fuga sequi nam eius quas excepturi
+        mollitia modi.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis facilis
+        eligendi voluptatibus obcaecati dolores delectus animi, harum iste alias
+        accusantium iusto numquam, dolore amet fugit ad, nesciunt eum. Possimus,
+        nisi!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere nobis
+        dolorum est rerum consequatur alias similique hic quaerat assumenda,
+        iure sunt vitae corrupti eveniet cupiditate quibusdam, impedit iste sed
+        quo?
+      </p>
+    </div>
+  );
+}
